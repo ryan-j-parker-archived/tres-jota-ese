a0e479e222004c9c8b8b3fc9bd7160b6e5eee500
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-09-18 00:16:57 -0700

    add planets, textures, and lighting

--- app.js
+++ app.js
@@ -15,20 +15,28 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const torus = new THREE.Mesh(geometry, material);
-scene.add(torus);
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(21, 1, 10);
 scene.add(pointLight);
 
+// const pointLight2 = new THREE.PointLight(0xffffff);
+// pointLight.position.set(11, 1, 20);
+// scene.add(pointLight2);
+
+// const pointLight3 = new THREE.PointLight(0xffffff);
+// pointLight.position.set(77, 1, 30);
+// scene.add(pointLight3);
+
+// const pointLight4 = new THREE.PointLight(0xffffff);
+// pointLight.position.set(42, 1, 50);
+// scene.add(pointLight4);
+
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(ambientLight);
 
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(gridHelper);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(gridHelper);
 
 const sphereSize = 1;
 const lightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
@@ -37,20 +45,6 @@ scene.add(lightHelper);
 const controls = new OrbitControls(camera, renderer.domElement);
 renderer.render(scene, camera);
 
-
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-
-    torus.rotation.x += 0.0025;
-    torus.rotation.y += 0.0025;
-    torus.rotation.x += 0.0025;
-
-    controls.update();
-    console.log(scene);
-}
-
-animate();
 
 function addStar() {
     const geometry = new THREE.SphereGeometry(0.25, 24, 24);
@@ -68,17 +62,48 @@ Array(200).fill().forEach(addStar);
 const pageTexture = new THREE.TextureLoader().load('../assets/stars.jpg');
 scene.background = pageTexture;
 
-const moonTexture = new THREE.TextureLoader().load('../assets/moon-water.jpg');
-
-const moon = new THREE.Mesh(
-    new THREE.BoxGeometry(3, 3, 3),
-    new THREE.MeshBasicMaterial({ map: moonTexture }),
-);
-
-scene.add(moon);
-
 const goldenWaterTexture = new THREE.TextureLoader().load('../assets/golden-water.jpg');
 const iceTexture = new THREE.TextureLoader().load('../assets/ice-texture.jpg');
+const moonTexture = new THREE.TextureLoader().load('../assets/moon-water.jpg');
+const grassTexture = new THREE.TextureLoader().load('./assets/grass.jpg');
+const shinyTexture = new THREE.TextureLoader().load('../assets/shiny-texture.jpg');
+const slabTexture = new THREE.TextureLoader().load('../assets/slab-texture.jpg');
+const paintTexture = new THREE.TextureLoader().load('../assets/paint-texture.jpg');
+const balls = new THREE.TextureLoader().load('../assets/balls.jpg');
+const brightPaint = new THREE.TextureLoader().load('../assets/bright-paint.jpg');
+const brightWater = new THREE.TextureLoader().load('../assets/bright-water.jpg');
+const shinyBright = new THREE.TextureLoader().load('../assets/shiny-bright.jpg');
+const bwShape = new THREE.TextureLoader().load('../assets/bw-shape.jpg');
+const space = new THREE.TextureLoader().load('../assets/space.jpg');
+const stoneTexture = new THREE.TextureLoader().load('../assets/stone.jpg');
+const space2 = new THREE.TextureLoader().load('../assets/space2.jpg');
+// const stars = new THREE.TextureLoader().load('../assets/stars.jpg');
+
+// planetary bodies
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(10, 3, 16, 100),
+    new THREE.MeshStandardMaterial({
+        map: stoneTexture,
+        normalMap: shinyBright,
+        // wireframe: true,
+    }),
+);
+torus.position.setX(-5);
+torus.position.setY(-5);
+torus.position.setZ(-25);
+scene.add(torus);
+
+const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(13, 43, 323),
+    new THREE.MeshBasicMaterial({
+        map: brightPaint,
+        normalMap: shinyBright,
+    }),
+);
+moon.position.setX(-110);
+moon.position.setY(90);
+moon.position.setZ(-280);
+scene.add(moon);
 
 const spheroid = new THREE.Mesh(
     new THREE.SphereGeometry(4, 32, 128),
@@ -87,26 +112,101 @@ const spheroid = new THREE.Mesh(
         normalMap: iceTexture,
     }),
 );
-
-const shinyTexture = new THREE.TextureLoader().load('../assets/shiny-texture.jpg');
-const slabTexture = new THREE.TextureLoader().load('../assets/slab-texture.jpg');
+spheroid.position.setX(55);
+spheroid.position.setY(15);
+spheroid.position.setZ(-65);
+scene.add(spheroid);
 
 const planet = new THREE.Mesh(
-    new THREE.SphereGeometry(6, 27, 99),
+    new THREE.SphereGeometry(6, 28, 99),
     new THREE.MeshStandardMaterial({
         map: shinyTexture,
-        normalMap: slabTexture, 
+        normalMap: slabTexture,
     })
 );
-
+planet.position.setX(35);
+planet.position.setY(-30);
+planet.position.setZ(-95);
 scene.add(planet);
 
-planet.position.z = -45;
-planet.position.setX(10);
-planet.position.setY(10);
+const donutHole = new THREE.Mesh(
+    new THREE.SphereGeometry(6, 29, 132),
+    new THREE.MeshStandardMaterial({
+        map: brightWater,
 
-spheroid.position.z = 30;
-spheroid.position.setX(5);
-spheroid.position.setY(-5);
+    }),
+);
+donutHole.position.setX(-5);
+donutHole.position.setY(-5);
+donutHole.position.setZ(-25);
+scene.add(donutHole);
 
-scene.add(spheroid);
+const torusKnot = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(10, 3, 100, 16),
+    new THREE.MeshBasicMaterial({
+        map: iceTexture,
+        normalMap: moonTexture,
+    }),
+);
+torusKnot.position.setX(-85);
+torusKnot.position.setY(-85);
+torusKnot.position.setZ(-105);
+scene.add(torusKnot);
+
+// animation function
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+
+    torus.rotation.x += 0.03;
+    torus.rotation.y += 0.03;
+    torus.rotation.z += 0.003;
+
+    moon.rotation.x += 0.04;
+    moon.rotation.y += 0.04;
+    moon.rotation.z += 0.04;
+
+    planet.rotation.x += .0001;
+    planet.rotation.y += .005;
+    planet.rotation.z += .0002;
+
+    spheroid.rotation.x -= .002;
+    spheroid.rotation.y += .0093;
+    spheroid.rotation.z -= .00009;
+
+    donutHole.rotation.x += 0.0005;
+    donutHole.rotation.y -= 0.05;
+    donutHole.rotation.z += 0.0005;
+
+    torusKnot.rotation.x += 0.003;
+    torusKnot.rotation.y += 0.003;
+    torusKnot.rotation.z += 0.0003;
+
+    controls.update();
+    console.log(scene);
+}
+
+animate();
+
+// change 
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+
+    moon.rotation.x += 0.05;
+    moon.rotation.y += 0.08;
+    moon.rotation.z += 0.05;
+
+    planet.rotation.x += 0.05;
+    planet.rotation.y += 0.08;
+    planet.rotation.z += 0.05;
+
+    spheroid.rotation.x += 0.05;
+    spheroid.rotation.y += 0.08;
+    spheroid.rotation.z += 0.05;
+
+    camera.position.setX(t * -0.00005);
+    camera.position.setY(t * -0.0002);
+    camera.position.setZ(t * -0.002);
+}
+
+document.body.onscroll = moveCamera;
