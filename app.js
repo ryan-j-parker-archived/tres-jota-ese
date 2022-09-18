60eda5261f534998a33a481814da2ae300c47c65
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-09-17 17:14:50 -0700

    add rendered torus with white mesh

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
-/* Imports */
+import * as THREE from 'https://unpkg.com/three@0.139.2/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.139.2/examples/jsm/controls/OrbitControls.js';
 
-/* Get DOM Elements */
+const scene = new THREE.Scene();
+// console.log(THREE);
 
-/* State */
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 30;
 
-/* Events */
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.getElementById('bg'),
+});
 
-/* Display Functions */
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-// (don't forget to call any display functions you want to run on page load!)
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+renderer.render(scene, camera);
+
+// camera.getWorldPosition.setZ(30);
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    console.log(scene);
+}
+
+animate();
